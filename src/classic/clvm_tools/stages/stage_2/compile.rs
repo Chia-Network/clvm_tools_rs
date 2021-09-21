16438328338f48e3addccd29eefbe7211b14f1eb
arty <art.yerkes@gmail.com>
2021-09-21 12:16:17 -0700

    trim parameters for lower_quote

--- src/classic/clvm_tools/stages/stage_2/compile.rs
+++ src/classic/clvm_tools/stages/stage_2/compile.rs
@@ -210,10 +210,7 @@ pub fn compile_symbols(
 // # Overrides symbol table defns.
 pub fn lower_quote(
     allocator: &mut Allocator,
-    prog: NodePtr,
-    macro_lookup: NodePtr,
-    symbol_table: NodePtr,
-    run_program: Rc<dyn TRunProgram>,
+    prog: NodePtr
 ) -> Result<NodePtr, EvalErr> {
     if prog == allocator.null() {
         return Ok(prog);
@@ -237,14 +234,7 @@ pub fn lower_quote(
                         }
 
                         return m! {
-                            lowered <-
-                                lower_quote(
-                                    allocator,
-                                    qlist[1],
-                                    macro_lookup,
-                                    symbol_table,
-                                    run_program
-                                );
+                            lowered <- lower_quote(allocator, qlist[1]);
                             quote(allocator, lowered)
                         };
                     } else {
@@ -256,22 +246,8 @@ pub fn lower_quote(
                         match allocator.sexp(prog) {
                             SExp::Pair(f,r) => {
                                 return m! {
-                                    first <-
-                                        lower_quote(
-                                            allocator,
-                                            f,
-                                            macro_lookup,
-                                            symbol_table,
-                                            run_program.clone()
-                                        );
-                                    rest <-
-                                        lower_quote(
-                                            allocator,
-                                            r,
-                                            macro_lookup,
-                                            symbol_table,
-                                            run_program.clone()
-                                        );
+                                    first <- lower_quote(allocator, f);
+                                    rest <- lower_quote(allocator, r);
                                     allocator.new_pair(first, rest)
                                 };
                             },
@@ -339,7 +315,10 @@ fn try_expand_macro_for_atom(
             run_program
         );
         Ok(res)
-    };
+    }.map_err(|x| {
+        print!("try_expand_macro returned error: CODE {} PROG_REST {}\n", disassemble(allocator, macro_code), disassemble(allocator, prog_rest));
+        x
+    });
 }
 
 fn get_macro_program(
@@ -478,7 +457,7 @@ enum SymbolResult {
 fn find_symbol_match(
     allocator: &mut Allocator,
     opname: &Vec<u8>,
-    compiled_args: Vec<NodePtr>,
+    r: NodePtr,
     symbol_table: NodePtr
 ) -> Result<Option<SymbolResult>, EvalErr> {
     let dissym = disassemble(allocator, symbol_table);
@@ -501,10 +480,7 @@ fn find_symbol_match(
 
                                 let symbuf = allocator.buf(&symptr).to_vec();
                                 if vec!('*' as u8) == symbuf {
-                                    return enlist(
-                                        allocator,
-                                        &compiled_args
-                                    ).map(|v| Some(SymbolResult::Direct(v)));
+                                    return Ok(Some(SymbolResult::Direct(r)));
                                 } else if *opname == symbuf {
                                     return Ok(Some(SymbolResult::Matched(symbol,value)));
                                 }
@@ -569,7 +545,7 @@ fn compile_application(
                     find_symbol_match(
                         allocator,
                         opbuf,
-                        compiled_args,
+                        r,
                         symbol_table
                     ).and_then(|x| match x {
                         Some(SymbolResult::Direct(v)) => { Ok(v) },
@@ -627,7 +603,13 @@ pub fn do_com_prog(
     symbol_table: NodePtr,
     run_program: Rc<dyn TRunProgram>
 ) -> Response {
-    do_com_prog_(allocator, prog, macro_lookup, symbol_table, run_program)
+    do_com_prog_(allocator, prog, macro_lookup, symbol_table, run_program).map_err(|x| {
+        print!("error in do_com_prog for {}\n", disassemble(allocator, prog));
+        x
+    }).map(|x| {
+        print!("DO_COM_PROG {} MACRO {} SYMBOLS {} RESULT {}\n", disassemble(allocator, prog), disassemble(allocator, macro_lookup), disassemble(allocator, symbol_table), disassemble(allocator, x.1));
+        x
+    })
 }
 
 fn do_com_prog_(
@@ -650,9 +632,7 @@ fn do_com_prog_(
 
     // lower "quote" to "q"
     return m! {
-        prog <- lower_quote(
-            allocator, prog, macro_lookup, symbol_table, run_program.clone()
-        );
+        prog <- lower_quote(allocator, prog);
 
         // quote atoms
         match allocator.sexp(prog) {
@@ -722,9 +702,9 @@ fn do_com_prog_(
                                 quoted_symbol_table
                             ));
 
-                            run_program.run_program(
-                                allocator, eval_list, top_atom, None
-                            ).and_then(|x| enlist(allocator, &vec!(x.1))).
+                            evaluate(
+                                allocator, eval_list, top_atom
+                            ).and_then(|x| enlist(allocator, &vec!(x))).
                                 map(|x| Reduction(1, x))
                         };
                     }
@@ -829,6 +809,25 @@ fn test_inner_expansion(
     return disassemble(allocator, exp_res);
 }
 
+fn test_do_com_prog(
+    allocator: &mut Allocator,
+    program_src: String,
+    macro_lookup_src: String,
+    symbol_table_src: String
+) -> String {
+    let runner = run_program_for_search_paths(&vec!(".".to_string()));
+    let prog_ir = read_ir(&program_src).unwrap();
+    let program = assemble_from_ir(allocator, Rc::new(prog_ir)).unwrap();
+    let macro_ir = read_ir(&macro_lookup_src).unwrap();
+    let macro_lookup = assemble_from_ir(allocator, Rc::new(macro_ir)).unwrap();
+    let sym_ir = read_ir(&symbol_table_src).unwrap();
+    let symbol_table = assemble_from_ir(allocator, Rc::new(sym_ir)).unwrap();
+    let result = do_com_prog(
+        allocator, program, macro_lookup, symbol_table, runner
+    ).unwrap();
+    return disassemble(allocator, result.1);
+}
+
 #[test]
 fn test_macro_expansion() {
     let mut allocator = Allocator::new();
@@ -851,4 +850,49 @@ fn test_inner_macro_exp() {
         "(\"function\" (\"BODY\") (29041 (\"opt\" (\"com\" (q \"unquote\" \"BODY\") (29041 (\"unquote\" (\"macros\"))) (29041 (\"unquote\" (\"symbols\")))))))".to_string()
     );
     assert_eq!(res, "(a (q 4 (q . \"list\") (c (f 1) (c (c (q . \"mod\") (c (f (r 1)) (c (f (r (r 1))) (q)))) (q)))) (q \"function\" (\"BODY\") (29041 (\"opt\" (\"com\" (q \"unquote\" \"BODY\") (29041 (\"unquote\" (\"macros\"))) (29041 (\"unquote\" (\"symbols\"))))))))".to_string());
+}
+
+#[test]
+fn test_compile_during_assert_1() {
+    let mut allocator = Allocator::new();
+    let res = test_do_com_prog(
+        &mut allocator,
+        "(\"assert\" 1)".to_string(),
+        "((\"assert\" (a (i 3 (q 4 (q . 26982) (c 2 (c (c (q . \"assert\") 3) (q (x))))) (q . 2)) 1)) (26982 (c (q . 2) (c (c (q . 3) (c 2 (c (c (q . \"function\") (c 5 ())) (c (c (q . \"function\") (c 11 ())) ())))) (q 64)))) (\"function\" (c (q . \"opt\") (c (c (q . \"com\") (c (c (q . 1) 2) (q (29041 (\"unquote\" (\"macros\"))) (29041 (\"unquote\" (\"symbols\")))))) ()))) (\"list\" (a (q 2 (q 2 2 (c 2 (c 3 (q)))) (c (q 2 (i 5 (q 4 (q . 4) (c 9 (c (a 2 (c 2 (c 13 (q)))) (q)))) (q 1)) 1) 1)) 1)) (\"defmacro\" (c (q . \"list\") (c (f 1) (c (c (q . \"mod\") (c (f (r 1)) (c (f (r (r 1))) (q)))) (q))))))".to_string(),
+        "()".to_string()
+    );
+
+    assert_eq!(res, "(a (\"com\" (a (q 2 (i 3 (q 4 (q . 26982) (c 2 (c (c (q . \"assert\") 3) (q (x))))) (q . 2)) 1) (q 1)) (q (\"assert\" (a (i 3 (q 4 (q . 26982) (c 2 (c (c (q . \"assert\") 3) (q (x))))) (q . 2)) 1)) (26982 (c (q . 2) (c (c (q . 3) (c 2 (c (c (q . \"function\") (c 5 ())) (c (c (q . \"function\") (c 11 ())) ())))) (q 64)))) (\"function\" (c (q . \"opt\") (c (c (q . \"com\") (c (c (q . 1) 2) (q (29041 (\"unquote\" (\"macros\"))) (29041 (\"unquote\" (\"symbols\")))))) ()))) (\"list\" (a (q 2 (q 2 2 (c 2 (c 3 (q)))) (c (q 2 (i 5 (q 4 (q . 4) (c 9 (c (a 2 (c 2 (c 13 (q)))) (q)))) (q 1)) 1) 1)) 1)) (\"defmacro\" (c (q . \"list\") (c (f 1) (c (c (q . \"mod\") (c (f (r 1)) (c (f (r (r 1))) (q)))) (q)))))) (q)) 1)".to_string());
+}
+
+#[test]
+fn test_compile_check_output_diag_assert() {
+    let mut allocator = Allocator::new();
+    let res = test_do_com_prog(
+        &mut allocator,
+        "(\"mod\" () (\"defmacro\" \"assert\" \"items\" (26982 (r \"items\") (\"list\" 26982 (f \"items\") (c \"assert\" (r \"items\")) (q 8)) (f \"items\"))) (\"assert\" 1))".to_string(),
+        "((26982 (c (q . 2) (c (c (q . 3) (c 2 (c (c (q . \"function\") (c 5 ())) (c (c (q . \"function\") (c 11 ())) ())))) (q 64)))) (\"function\" (c (q . \"opt\") (c (c (q . \"com\") (c (c (q . 1) 2) (q (29041 (\"unquote\" (\"macros\"))) (29041 (\"unquote\" (\"symbols\")))))) ()))) (\"list\" (a (q 2 (q 2 2 (c 2 (c 3 (q)))) (c (q 2 (i 5 (q 4 (q . 4) (c 9 (c (a 2 (c 2 (c 13 (q)))) (q)))) (q 1)) 1) 1)) 1)) (\"defmacro\" (c (q . \"list\") (c (f 1) (c (c (q . \"mod\") (c (f (r 1)) (c (f (r (r 1))) (q)))) (q))))))".to_string(),
+        "()".to_string()
+    );
+
+    assert_eq!(
+        res,
+        "(a (q \"opt\" (q 2 (\"opt\" (\"com\" (q \"assert\" 1) (q (\"assert\" (a (i 3 (q 4 (q . 26982) (c 2 (c (c (q . \"assert\") 3) (q (x))))) (q . 2)) 1)) (26982 (c (q . 2) (c (c (q . 3) (c 2 (c (c (q . \"function\") (c 5 ())) (c (c (q . \"function\") (c 11 ())) ())))) (q 64)))) (\"function\" (c (q . \"opt\") (c (c (q . \"com\") (c (c (q . 1) 2) (q (29041 (\"unquote\" (\"macros\"))) (29041 (\"unquote\" (\"symbols\")))))) ()))) (\"list\" (a (q 2 (q 2 2 (c 2 (c 3 (q)))) (c (q 2 (i 5 (q 4 (q . 4) (c 9 (c (a 2 (c 2 (c 13 (q)))) (q)))) (q 1)) 1) 1)) 1)) (\"defmacro\" (c (q . \"list\") (c (f 1) (c (c (q . \"mod\") (c (f (r 1)) (c (f (r (r 1))) (q)))) (q)))))) (q))) 1)) 1)".to_string()
+    );
+}
+
+#[test]
+fn test_compile_assert_2() {
+    let mut allocator = Allocator::new();
+    let res = test_do_com_prog(
+        &mut allocator,
+        "(\"mod\" () (\"defmacro\" \"assert\" \"items\" (26982 (r \"items\") (\"list\" 26982 (f \"items\") (c \"assert\" (r \"items\")) (q 8)) (f \"items\"))) (\"assert\" 1))".to_string(),
+        "((26982 (c (q . 2) (c (c (q . 3) (c 2 (c (c (q . \"function\") (c 5 ())) (c (c (q . \"function\") (c 11 ())) ())))) (q 64)))) (\"function\" (c (q . \"opt\") (c (c (q . \"com\") (c (c (q . 1) 2) (q (29041 (\"unquote\" (\"macros\"))) (29041 (\"unquote\" (\"symbols\")))))) ()))) (\"list\" (a (q 2 (q 2 2 (c 2 (c 3 (q)))) (c (q 2 (i 5 (q 4 (q . 4) (c 9 (c (a 2 (c 2 (c 13 (q)))) (q)))) (q 1)) 1) 1)) 1)) (\"defmacro\" (c (q . \"list\") (c (f 1) (c (c (q . \"mod\") (c (f (r 1)) (c (f (r (r 1))) (q)))) (q))))))".to_string(),
+        "()".to_string()
+    );
+
+    assert_eq!(
+        res,
+        "(a (q \"opt\" (q 2 (\"opt\" (\"com\" (q \"assert\" 1) (q (\"assert\" (a (i 3 (q 4 (q . 26982) (c 2 (c (c (q . \"assert\") 3) (q (x))))) (q . 2)) 1)) (26982 (c (q . 2) (c (c (q . 3) (c 2 (c (c (q . \"function\") (c 5 ())) (c (c (q . \"function\") (c 11 ())) ())))) (q 64)))) (\"function\" (c (q . \"opt\") (c (c (q . \"com\") (c (c (q . 1) 2) (q (29041 (\"unquote\" (\"macros\"))) (29041 (\"unquote\" (\"symbols\")))))) ()))) (\"list\" (a (q 2 (q 2 2 (c 2 (c 3 (q)))) (c (q 2 (i 5 (q 4 (q . 4) (c 9 (c (a 2 (c 2 (c 13 (q)))) (q)))) (q 1)) 1) 1)) 1)) (\"defmacro\" (c (q . \"list\") (c (f 1) (c (c (q . \"mod\") (c (f (r 1)) (c (f (r (r 1))) (q)))) (q)))))) (q))) 1)) 1)".to_string()
+    );
 }
