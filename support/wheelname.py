8baa67f203b065d070cb651f251227cd79c36d00
arty <art.yerkes@gmail.com>
2022-01-09 21:40:40 -0800

    Opps, i understand what's going on here a bit better ... try installing from inside the python script to sidestep a problem if it exists sourcing the output script

--- support/wheelname.py
+++ support/wheelname.py
@@ -1,6 +1,7 @@
 import os
+import subprocess
 
-with open('usewheel.ps1', 'w') as f:
-    wheels_files = os.listdir('target/wheels')
-    wheelname = list(filter(lambda x: x.endswith('whl'), wheels_files))[0]
-    f.write('$env:WHEEL_PATH="target/wheels/%s"\n' % wheelname)
+wheels_files = os.listdir('target/wheels')
+wheelname = list(filter(lambda x: x.endswith('whl'), wheels_files))[0]
+filename = "target/wheels/%s" % wheelname
+subprocess.check_call("pip install %s" % filename)
