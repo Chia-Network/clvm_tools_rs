93daaaecd2e4617b51c3f504a3fe18424c16931e
arty <art.yerkes@gmail.com>
2022-05-17 05:49:53 -0700

    improve support script

--- support/wheelname.py
+++ support/wheelname.py
@@ -4,4 +4,5 @@ import subprocess
 wheels_files = os.listdir('target/wheels')
 wheelname = list(filter(lambda x: x.endswith('whl') and 'musl' not in x, wheels_files))[0]
 filename = "target/wheels/%s" % wheelname
-subprocess.check_call("pip install %s" % filename)
+print(filename)
+subprocess.check_call(["pip", "install", filename])
