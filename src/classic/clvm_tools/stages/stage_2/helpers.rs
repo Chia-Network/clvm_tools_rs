df5ea616088dbf3455a75aaa5a0db19ff9a890ed
arty <art.yerkes@gmail.com>
2022-08-16 01:04:36 -0700

    More

--- src/classic/clvm_tools/stages/stage_2/helpers.rs
+++ src/classic/clvm_tools/stages/stage_2/helpers.rs
@@ -5,12 +5,12 @@ use crate::classic::clvm::sexp::enlist;
 use crate::classic::clvm_tools::node_path::NodePath;
 
 lazy_static! {
-    pub static ref QUOTE_ATOM: Vec<u8> = vec!(1);
-    pub static ref APPLY_ATOM: Vec<u8> = vec!(2);
-    pub static ref COM_ATOM: Vec<u8> = vec!('c' as u8, 'o' as u8, 'm' as u8);
+    pub static ref QUOTE_ATOM: Vec<u8> = vec![1];
+    pub static ref APPLY_ATOM: Vec<u8> = vec![2];
+    pub static ref COM_ATOM: Vec<u8> = vec![b'c', b'o', b'm'];
 }
 
-pub fn quote<'a>(allocator: &'a mut Allocator, sexp: NodePtr) -> Result<NodePtr, EvalErr> {
+pub fn quote(allocator: &mut Allocator, sexp: NodePtr) -> Result<NodePtr, EvalErr> {
     allocator
         .new_atom(&QUOTE_ATOM)
         .and_then(|q| allocator.new_pair(q, sexp))
@@ -18,8 +18,8 @@ pub fn quote<'a>(allocator: &'a mut Allocator, sexp: NodePtr) -> Result<NodePtr,
 
 // In original python code, the name of this function is `eval`,
 // but since the name `eval` cannot be used in typescript context, change the name to `evaluate`.
-pub fn evaluate<'a>(
-    allocator: &'a mut Allocator,
+pub fn evaluate(
+    allocator: &mut Allocator,
     prog: NodePtr,
     args: NodePtr,
 ) -> Result<NodePtr, EvalErr> {
@@ -29,8 +29,8 @@ pub fn evaluate<'a>(
     };
 }
 
-pub fn run<'a>(
-    allocator: &'a mut Allocator,
+pub fn run(
+    allocator: &mut Allocator,
     prog: NodePtr,
     macro_lookup: NodePtr,
 ) -> Result<NodePtr, EvalErr> {
@@ -44,14 +44,14 @@ pub fn run<'a>(
     return m! {
         mac <- quote(allocator, macro_lookup);
         com_sexp <- allocator.new_atom(&COM_ATOM);
-        arg_sexp <- allocator.new_atom(&args.data());
+        arg_sexp <- allocator.new_atom(args.data());
         to_eval <- enlist(allocator, &vec!(com_sexp, prog, mac));
         evaluate(allocator, to_eval, arg_sexp)
     };
 }
 
-pub fn brun<'a>(
-    allocator: &'a mut Allocator,
+pub fn brun(
+    allocator: &mut Allocator,
     prog: NodePtr,
     args: NodePtr,
 ) -> Result<NodePtr, EvalErr> {
