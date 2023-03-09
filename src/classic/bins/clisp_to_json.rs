f197a7cdf9f3872501acef0eb6913e64a3bd1892
Adam Kelly <338792+aqk@users.noreply.github.com>
2023-03-08 16:42:45 -0800

    Remove unused lines

--- src/classic/bins/clisp_to_json.rs
+++ src/classic/bins/clisp_to_json.rs
@@ -1,24 +1,17 @@
 extern crate clvmr as clvm_rs;
 
 use ::serde_json;
-//use std::collections::HashMap;
 use std::env;
 use std::rc::Rc;
 
-//use clvm_rs::allocator::Allocator;
-
 use clvm_tools_rs::compiler::compiler::DefaultCompilerOpts;
-//use clvm_tools_rs::compiler::evaluate::{Evaluator, EVAL_STACK_LIMIT};
 use clvm_tools_rs::compiler::frontend::frontend;
 use clvm_tools_rs::compiler::sexp::parse_sexp;
 use clvm_tools_rs::compiler::srcloc::Srcloc;
 
-//use clvm_tools_rs::classic::clvm_tools::stages::stage_0::DefaultProgramRunner;
 use clvm_tools_rs::util::ErrInto;
 
 fn main() {
-    //let mut allocator = Allocator::new();
-    //let runner = Rc::new(DefaultProgramRunner::new());
     let opts = Rc::new(DefaultCompilerOpts::new("*program*"));
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
