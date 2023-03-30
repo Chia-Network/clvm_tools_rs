9686d424596900c1e33f6df1d3afe4933ba2bd13
arty <art.yerkes@gmail.com>
2023-03-30 12:26:33 -0700

    Add basic coverage scripts, to start doing some coverage testing

---
+++ resources/coverage/run_coverage.py
@@ -0,0 +1,46 @@
+import os
+import json
+import shutil
+import subprocess
+from pathlib import Path
+import distill_coverage_results
+
+env = dict(os.environ)
+env['RUSTFLAGS'] = "-C instrument-coverage"
+env['LLVM_PROFILE_FILE'] = "your_name-%p-%m.profraw"
+
+def get_profile_files(the_dir):
+    return list(filter(lambda x: x.endswith('.profraw'), os.listdir(the_dir)))
+
+def delete_coverage_files():
+    for the_file in get_profile_files('.'):
+        if the_file.endswith('.profraw'):
+            os.unlink(the_file)
+
+    try:
+        os.unlink('coverage.json')
+    except:
+        pass
+
+def run_coverage_test():
+    subprocess.check_call(['cargo','test'],env=env)
+
+def is_my_code(desc):
+    return not('.cargo' in desc['name']) and not('library/std' in desc['name'])
+
+def collect_coverage():
+    profile_files = get_profile_files('.')
+    args = ['grcov','--binary-path','target/debug','--output-type','covdir','--output-path','coverage.json'] + profile_files
+    subprocess.check_call(args)
+    result = distill_coverage_results.CoverageResult()
+
+    infile = json.loads(open('coverage.json').read())
+    for root in infile['children'].keys():
+        distill_coverage_results.distill_coverage(result, Path(root), infile['children'][root])
+    return list(filter(is_my_code, result.show_result()))
+
+if __name__ == '__main__':
+    delete_coverage_files()
+    run_coverage_test()
+    result = collect_coverage()
+    print(json.dumps(result, indent=4))
