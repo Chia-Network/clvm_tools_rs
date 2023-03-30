f61035d87ae134d9b75a1ca1bd3d51581d3f9aef
arty <art.yerkes@gmail.com>
2023-03-30 12:50:22 -0700

    Improve run_coverage a bit

--- resources/coverage/run_coverage.py
+++ resources/coverage/run_coverage.py
@@ -9,6 +9,8 @@ env = dict(os.environ)
 env['RUSTFLAGS'] = "-C instrument-coverage"
 env['LLVM_PROFILE_FILE'] = "your_name-%p-%m.profraw"
 
+coverage_file = 'coverage.json'
+
 def get_profile_files(the_dir):
     return list(filter(lambda x: x.endswith('.profraw'), os.listdir(the_dir)))
 
@@ -18,7 +20,7 @@ def delete_coverage_files():
             os.unlink(the_file)
 
     try:
-        os.unlink('coverage.json')
+        os.unlink(coverage_file)
     except:
         pass
 
@@ -26,15 +28,15 @@ def run_coverage_test():
     subprocess.check_call(['cargo','test'],env=env)
 
 def is_my_code(desc):
-    return not('.cargo' in desc['name']) and not('library/std' in desc['name'])
+    return not('.cargo' in desc['name']) and not('library/std' in desc['name']) and not('src/classic/bins' in desc['name'])
 
 def collect_coverage():
     profile_files = get_profile_files('.')
-    args = ['grcov','--binary-path','target/debug','--output-type','covdir','--output-path','coverage.json'] + profile_files
+    args = ['grcov','--binary-path','target/debug','--output-type','covdir','--output-path',coverage_file] + profile_files
     subprocess.check_call(args)
     result = distill_coverage_results.CoverageResult()
 
-    infile = json.loads(open('coverage.json').read())
+    infile = json.loads(open(coverage_file).read())
     for root in infile['children'].keys():
         distill_coverage_results.distill_coverage(result, Path(root), infile['children'][root])
     return list(filter(is_my_code, result.show_result()))
