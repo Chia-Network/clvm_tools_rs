e19f00e40ac39571fd93f2b703fb50c811b013fa
arty <art.yerkes@gmail.com>
2023-03-30 17:23:54 -0700

    Pull coverage in clvmc.rs up

---
+++ src/tests/classic/clvmc.rs
@@ -0,0 +1,27 @@
+use std::collections::HashMap;
+use std::fs;
+
+use crate::classic::clvm_tools::clvmc::compile_clvm;
+use crate::tests::classic::run::read_json_from_file;
+
+#[test]
+fn test_compile_clvm_function_1() {
+    let mut symbol_hash_table = HashMap::new();
+    let bridge_hex_file = "validation_taproot.clvm.hex";
+    let _ = compile_clvm(
+        "resources/tests/bridgeref/validation_taproot.clsp",
+        bridge_hex_file,
+        &["resources/tests/bridge-includes".to_string()],
+        &mut symbol_hash_table
+    ).expect("should compile");
+    let bridge_hex = fs::read_to_string(bridge_hex_file).expect("should have been created");
+    fs::remove_file(bridge_hex_file).expect("should have existed");
+    let read_in_hex_data = fs::read_to_string("resources/tests/bridgeref/validation_taproot.clvm.hex.reference").expect("should exist");
+    let read_in_symbol_data = read_json_from_file("resources/tests/bridgeref/validation_taproot.sym.reference");
+    assert_eq!(bridge_hex, read_in_hex_data);
+    for (key, value) in read_in_symbol_data.iter() {
+        if key != "source_file" {
+            assert_eq!(symbol_hash_table.get(key), Some(value));
+        }
+    }
+}
