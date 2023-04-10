9a279d321e1346447d0276d8114b4948c902642c
arty <art.yerkes@gmail.com>
2023-04-10 14:44:12 -0700

    Remove unused import

--- src/classic/clvm_tools/stages/stage_2/operators.rs
+++ src/classic/clvm_tools/stages/stage_2/operators.rs
@@ -17,7 +17,7 @@ use crate::classic::clvm::keyword_from_atom;
 use crate::classic::clvm::sexp::proper_list;
 
 use crate::classic::clvm_tools::binutils::{
-    assemble_from_ir, disassemble, disassemble_to_ir_with_kw,
+    assemble_from_ir, disassemble_to_ir_with_kw,
 };
 use crate::classic::clvm_tools::ir::reader::read_ir;
 use crate::classic::clvm_tools::ir::writer::write_ir_to_stream;
