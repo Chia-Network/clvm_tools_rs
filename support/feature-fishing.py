5511dcca0afa94ce988ce5858a8f1bd6995765fb
arty <art.yerkes@gmail.com>
2023-05-19 17:07:15 -0700

    Support a simpler style if desired, use a direct append in windows

--- support/feature-fishing.py
+++ support/feature-fishing.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python
 
 from tomlkit import parse
+import sys
 
 cargo_toml = parse(open('Cargo.toml').read())
-print(",".join(list(filter(lambda x: x != 'extension-module', cargo_toml['features']['default']))))
+results=",".join(list(filter(lambda x: x != 'extension-module', cargo_toml['features']['default'])))
+if len(sys.argv) > 1:
+    print(f"{sys.argv[1]}{results}")
+else:
+    print(results)
