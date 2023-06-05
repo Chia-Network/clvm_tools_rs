8fcaaafe3d3b60e1c8425b8c188acfb947d2cea1
arty <art.yerkes@gmail.com>
2023-06-05 11:25:42 -0700

    Refine a bit

--- src/compiler/optimize/above22.rs
+++ src/compiler/optimize/above22.rs
@@ -1,4 +1,3 @@
-use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::rc::Rc;
 
@@ -10,36 +9,32 @@ use crate::compiler::comptypes::{
     BodyForm, CompileErr, CompileForm, CompilerOpts, DefunData, HelperForm, PrimaryCodegen,
 };
 use crate::compiler::optimize::{
-    deinline_opt, fe_opt, null_optimization, optimize_expr, run_optimizer,
+    deinline_opt, optimize_expr, null_optimization, run_optimizer,
     CodegenOptimizationResult, CompileContextWrapper, Optimization,
 };
 use crate::compiler::sexp::SExp;
 
-/// Captures the existing strategy set for cl22 and below.
+/// Captures the strategy for cl23 and above.
+/// Until formally released, we can take action in here.
 #[derive(Default, Clone)]
-pub struct ExistingStrategy {}
+pub struct Strategy23 {}
 
-impl ExistingStrategy {
+impl Strategy23 {
     pub fn new() -> Self {
-        ExistingStrategy {}
+        Strategy23 {}
     }
 }
 
-impl Optimization for ExistingStrategy {
+impl Optimization for Strategy23 {
     fn frontend_optimization(
         &mut self,
-        allocator: &mut Allocator,
-        runner: Rc<dyn TRunProgram>,
-        opts: Rc<dyn CompilerOpts>,
+        _allocator: &mut Allocator,
+        _runner: Rc<dyn TRunProgram>,
+        _opts: Rc<dyn CompilerOpts>,
         p0: CompileForm,
     ) -> Result<CompileForm, CompileErr> {
         // Not yet turned on for >22
-        if opts.frontend_opt() && !(opts.dialect().stepping.map(|d| d > 22).unwrap_or(false)) {
-            // Front end optimization
-            fe_opt(allocator, runner, opts.clone(), p0)
-        } else {
-            Ok(p0)
-        }
+        Ok(p0)
     }
 
     fn post_desugar_optimization(
@@ -70,8 +65,16 @@ impl Optimization for ExistingStrategy {
         &mut self,
         _code_generator: &PrimaryCodegen,
         _hf: Option<HelperForm>,
-        _repr: Rc<SExp>,
+        repr: Rc<SExp>,
     ) -> Result<CodegenOptimizationResult, CompileErr> {
+        let (worked, result) = null_optimization(repr, true);
+        if worked {
+            return Ok(CodegenOptimizationResult {
+                code: Some(result),
+                .. Default::default()
+            });
+        }
+
         Ok(Default::default())
     }
 
@@ -79,14 +82,10 @@ impl Optimization for ExistingStrategy {
         &mut self,
         allocator: &mut Allocator,
         runner: Rc<dyn TRunProgram>,
-        opts: Rc<dyn CompilerOpts>,
+        _opts: Rc<dyn CompilerOpts>,
         code: Rc<SExp>,
     ) -> Result<Rc<SExp>, CompileErr> {
-        if opts.optimize() {
-            run_optimizer(allocator, runner, code)
-        } else {
-            Ok(code)
-        }
+        run_optimizer(allocator, runner, code)
     }
 
     fn defun_body_optimization(
@@ -97,35 +96,26 @@ impl Optimization for ExistingStrategy {
         codegen: &PrimaryCodegen,
         defun: &DefunData,
     ) -> Result<Rc<BodyForm>, CompileErr> {
-        let res = if opts.optimize() {
-            // Run optimizer on frontend style forms.
-            optimize_expr(
-                allocator,
-                opts.clone(),
-                runner.clone(),
-                codegen,
-                defun.body.clone(),
-            )
+        // Run optimizer on frontend style forms.
+        Ok(optimize_expr(
+            allocator,
+            opts.clone(),
+            runner.clone(),
+            codegen,
+            defun.body.clone(),
+        )
             .map(|x| x.1)
-            .unwrap_or_else(|| defun.body.clone())
-        } else {
-            defun.body.clone()
-        };
-        Ok(res)
+            .unwrap_or_else(|| defun.body.clone()))
     }
 
     fn post_codegen_function_optimize(
         &mut self,
         allocator: &mut Allocator,
         runner: Rc<dyn TRunProgram>,
-        opts: Rc<dyn CompilerOpts>,
+        _opts: Rc<dyn CompilerOpts>,
         code: Rc<SExp>,
     ) -> Result<Rc<SExp>, CompileErr> {
-        if opts.optimize() {
-            run_optimizer(allocator, runner, code)
-        } else {
-            Ok(code)
-        }
+        run_optimizer(allocator, runner, code)
     }
 
     fn pre_final_codegen_optimize(
@@ -135,36 +125,22 @@ impl Optimization for ExistingStrategy {
         opts: Rc<dyn CompilerOpts>,
         codegen: &PrimaryCodegen,
     ) -> Result<Rc<BodyForm>, CompileErr> {
-        let res = if opts.optimize() {
-            optimize_expr(
-                allocator,
-                opts.clone(),
-                runner,
-                codegen,
-                codegen.final_expr.clone(),
-            )
+        Ok(optimize_expr(
+            allocator,
+            opts.clone(),
+            runner,
+            codegen,
+            codegen.final_expr.clone(),
+        )
             .map(|x| x.1)
-            .unwrap_or_else(|| codegen.final_expr.clone())
-        } else {
-            codegen.final_expr.clone()
-        };
-
-        Ok(res)
+            .unwrap_or_else(|| codegen.final_expr.clone()))
     }
 
     fn post_codegen_output_optimize(
         &mut self,
-        opts: Rc<dyn CompilerOpts>,
+        _opts: Rc<dyn CompilerOpts>,
         generated: SExp,
     ) -> Result<SExp, CompileErr> {
-        if opts.frontend_opt() && opts.dialect().stepping.map(|s| s > 22).unwrap_or(false) {
-            let (did_work, optimized) = null_optimization(Rc::new(generated.clone()), false);
-            if did_work {
-                let o_borrowed: &SExp = optimized.borrow();
-                return Ok(o_borrowed.clone());
-            }
-        }
-
         Ok(generated)
     }
 
