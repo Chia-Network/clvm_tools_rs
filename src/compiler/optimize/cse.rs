dea2617ea1c26fea90292435cb4050d94249b702
arty <art.yerkes@gmail.com>
2023-06-10 21:40:57 -0700

    Full implementation of CSE optimization

--- src/compiler/optimize/cse.rs
+++ src/compiler/optimize/cse.rs
@@ -3,12 +3,12 @@ use std::collections::{HashMap, HashSet};
 use std::rc::Rc;
 
 use crate::compiler::clvm::sha256tree;
-use crate::compiler::comptypes::{BodyForm, CompileErr};
-use crate::compiler::evaluate::is_apply_atom;
+use crate::compiler::comptypes::{Binding, BindingPattern, BodyForm, CompileErr, DefunData, HelperForm, LetFormInlineHint, LetFormKind, LetData};
+use crate::compiler::gensym::gensym;
 use crate::compiler::optimize::bodyform::{
-    path_overlap_one_way, visit_detect_in_bodyform, BodyformPathArc, PathDetectVisitorResult,
+    path_overlap_one_way, visit_detect_in_bodyform, BodyformPathArc, PathDetectVisitorResult, retrieve_bodyform, replace_in_bodyform,
 };
-use crate::compiler::sexp::SExp;
+use crate::compiler::sexp::{decode_string, SExp};
 
 // Common subexpression elimintation.
 // catalog subexpressions of the given bodyform and
@@ -22,14 +22,6 @@ pub struct CSEDetection {
     pub hash: Vec<u8>,
     pub subexp: BodyForm,
     pub instances: Vec<CSEInstance>,
-}
-
-fn is_apply(bf: &BodyForm) -> bool {
-    if let BodyForm::Call(_, parts) = bf {
-        return parts.len() == 3 && is_apply_atom(parts[0].to_sexp());
-    }
-
-    false
 }
 
 fn is_constant(bf: &BodyForm) -> bool {
@@ -69,7 +61,7 @@ fn is_fully_dominated(cse: &CSEDetection, detections: &[CSEDetection]) -> bool {
 
 pub fn cse_detect(fe: Rc<BodyForm>) -> Result<Vec<CSEDetection>, CompileErr> {
     let found_exprs = visit_detect_in_bodyform(
-        &|path, original, form| {
+        &|path, _original, form| {
             // The whole expression isn't a repeat.
             if path.is_empty() {
                 return Ok(None);
@@ -136,4 +128,174 @@ pub fn cse_detect(fe: Rc<BodyForm>) -> Result<Vec<CSEDetection>, CompileErr> {
         .collect();
 
     Ok(useful_detections)
+}
+
+// Number of other CSE detections this one depends on.
+// We can't apply it until the ones it depends on are applied.
+fn number_of_overlaps(detections: &[CSEDetection], cse: &CSEDetection) -> usize {
+    cse.instances
+        .iter()
+        .map(|i: &CSEInstance| {
+            detections
+                .iter()
+                .filter(|d| d.hash != cse.hash)
+                .map(|d| {
+                    d.instances
+                        .iter()
+                        .filter(|j: &&CSEInstance| path_overlap_one_way(&i.path, &j.path))
+                        .count()
+                })
+                .sum::<usize>()
+        })
+        .sum()
+}
+
+fn sorted_cse_detections_by_applicability(
+    cse_detections: &[CSEDetection],
+) -> Vec<(usize, CSEDetection)> {
+    let mut detections_with_dependencies: Vec<(usize, CSEDetection)> = cse_detections
+        .iter()
+        .map(|a| (number_of_overlaps(&cse_detections, a), a.clone()))
+        .collect();
+    detections_with_dependencies.sort_by(|(a, _), (b, _)| a.cmp(b));
+    detections_with_dependencies
+}
+
+pub fn cse_optimize_bodyform(
+    h: &HelperForm,
+    d: &DefunData,
+) -> Result<Rc<BodyForm>, CompileErr> {
+    let cse_detections = cse_detect(d.body.clone())?;
+    eprintln!("cse_detections {}", cse_detections.len());
+
+    // While we have them, apply any detections that overlap no others.
+    let mut detections_with_dependencies: Vec<(usize, CSEDetection)> =
+        sorted_cse_detections_by_applicability(&cse_detections);
+
+    let mut function_body = d.body.clone();
+    let mut new_binding_stack = Vec::new();
+
+    while !detections_with_dependencies.is_empty() {
+        let detections_to_apply: Vec<CSEDetection> = detections_with_dependencies
+            .iter()
+            .take_while(|(c, _d)| *c == 0)
+            .map(|(_c, d)| d.clone())
+            .collect();
+        let keep_detections: Vec<CSEDetection> = detections_with_dependencies
+            .iter()
+            .skip_while(|(c, _d)| *c == 0)
+            .map(|(_c, d)| d.clone())
+            .collect();
+
+        // It's an error if applications are deadlocked.
+        // I don't think it's possible but this will prevent infinite
+        // looping.
+        if detections_to_apply.is_empty() && !keep_detections.is_empty() {
+            return Err(CompileErr(
+                h.loc(),
+                format!(
+                    "CSE optimization failed in helper {}",
+                    decode_string(h.name())
+                ),
+            ));
+        }
+
+        let mut binding_set = Vec::new();
+
+        for d in detections_to_apply.iter() {
+            // If for some reason there are none to apply, we can
+            // skip it.  That should not be possible.
+            if d.instances.is_empty() {
+                break;
+            }
+
+            // All detections should have been transformed equally.
+            // Therefore, we can pick one out and use its form.
+            //
+            // These might have changed from when they were detected
+            // because other common subexpressions were substuted.
+            let prototype_instance = if let Some(r) = retrieve_bodyform(
+                &d.instances[0].path,
+                &function_body,
+                &|b: &BodyForm| b.clone(),
+            ) {
+                r
+            } else {
+                return Err(CompileErr(
+                    h.loc(),
+                    format!(
+                        "CSE Error in {}: could not find form to replace for path {:?}",
+                        decode_string(h.name()),
+                        d.instances[0].path
+                    ),
+                ));
+            };
+
+            // We'll assign a fresh variable for each of the detections
+            // that are applicable now.
+            let new_variable_name = gensym(b"cse".to_vec());
+            let new_variable_bf = BodyForm::Value(SExp::Atom(
+                prototype_instance.loc(),
+                new_variable_name.clone(),
+            ));
+
+            let replacement_spec: Vec<PathDetectVisitorResult<()>> = d
+                .instances
+                .iter()
+                .map(|i| PathDetectVisitorResult {
+                    path: i.path.clone(),
+                    subexp: new_variable_bf.clone(),
+                    context: (),
+                })
+                .collect();
+
+            if let Some(res) = replace_in_bodyform(
+                &replacement_spec,
+                function_body.borrow(),
+                &|v: &PathDetectVisitorResult<()>, _b| v.subexp.clone(),
+            ) {
+                function_body = Rc::new(res);
+            } else {
+                return Err(CompileErr(h.loc(), format!("cse replacement failed in helper {}, which shouldn't be possible", decode_string(h.name()))));
+            }
+
+            // Put aside the definition in this binding set.
+            binding_set.push((new_variable_name, prototype_instance));
+
+            detections_with_dependencies =
+                sorted_cse_detections_by_applicability(&keep_detections);
+        }
+
+        new_binding_stack.push(
+            binding_set
+                .iter()
+                .map(|(name, body)| {
+                    let name_atom = SExp::Atom(body.loc(), name.clone());
+                    Rc::new(Binding {
+                        loc: body.loc(),
+                        nl: body.loc(),
+                        pattern: BindingPattern::Complex(Rc::new(name_atom)),
+                        body: Rc::new(body.clone()),
+                    })
+                })
+                .collect(),
+        );
+    }
+
+    // All CSE replacements are done.  We unwind the new bindings
+    // into a stack of parallel let forms.
+    for binding_list in new_binding_stack.into_iter() {
+        function_body = Rc::new(BodyForm::Let(
+            LetFormKind::Parallel,
+            Box::new(LetData {
+                loc: function_body.loc(),
+                kw: None,
+                inline_hint: Some(LetFormInlineHint::NonInline(h.loc())),
+                bindings: binding_list,
+                body: function_body,
+            }),
+        ));
+    }
+
+    Ok(function_body)
 }
