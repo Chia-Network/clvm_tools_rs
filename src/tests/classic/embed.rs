d45e5f3219972984311ef5d44102dbb520f8d6a9
arty <art.yerkes@gmail.com>
2023-08-02 13:43:11 -0700

    more tests

--- src/tests/classic/embed.rs
+++ src/tests/classic/embed.rs
@@ -13,6 +13,26 @@ const BIN_TYPE: &str = "bin";
 const HEX_TYPE: &str = "hex";
 const SEXP_TYPE: &str = "sexp";
 
+#[test]
+fn test_embed_file_as_variable_name() {
+    let program = indoc! {"
+(mod (X) (defun embed-file (embed-file) (+ 1 embed-file)) (embed-file X))
+    "}
+    .to_string();
+    let compiled = do_basic_run(&vec![
+        "run".to_string(),
+        "-i".to_string(),
+        "resources/tests".to_string(),
+        program,
+    ]);
+
+    let result = do_basic_brun(&vec!["brun".to_string(), compiled, "(103)".to_string()])
+        .trim()
+        .to_string();
+
+    assert_eq!(result, "104");
+}
+
 // Embed test matrix (suggested)
 // Exists, NotExists vs Bin, Hex, Sexp vs Modern, Classic
 // - Exists
@@ -37,39 +57,53 @@ fn test_embed_exhaustive() {
         (SEXP_TYPE, EMBED_SEXP_FILE, SEXP_HASH),
     ];
 
-    for exists in 0..=1 {
-        for (include_kind, include_file, want_hash) in include_list.iter() {
-            for modern in 0..=1 {
-                let modern_sigil = if modern > 0 {
-                    "(include *standard-cl-21*)"
-                } else {
-                    ""
-                };
-                let filename = if exists > 0 {
-                    include_file
-                } else {
-                    EMBED_NOT_EXIST
-                };
-
-                let program = format!("(mod () {modern_sigil} (include sha256tree.clib) (embed-file embedded-data {include_kind} {filename}) (sha256tree embedded-data))");
-                let compiled = do_basic_run(&vec![
-                    "run".to_string(),
-                    "-i".to_string(),
-                    "resources/tests".to_string(),
-                    program,
-                ]);
-
-                if exists > 0 {
-                    let output =
-                        do_basic_brun(&vec!["brun".to_string(), compiled, "()".to_string()])
-                            .trim()
-                            .to_string();
-                    assert_eq!(want_hash, &output);
-                } else {
-                    if modern > 0 {
-                        assert!(compiled.contains("could not find not-exist.txt"));
+    for order in 0..=1 {
+        for exists in 0..=1 {
+            for (include_kind, include_file, want_hash) in include_list.iter() {
+                for modern in 0..=1 {
+                    let modern_sigil = if modern > 0 {
+                        "(include *standard-cl-21*)"
                     } else {
-                        assert!(compiled.starts_with("FAIL:"));
+                        ""
+                    };
+                    let filename = if exists > 0 {
+                        include_file
+                    } else {
+                        EMBED_NOT_EXIST
+                    };
+
+                    let embed_pt: Vec<String> = (0..=1)
+                        .map(|i| {
+                            if i == order {
+                                format!("(embed-file embedded-data {include_kind} {filename})")
+                            } else {
+                                "".to_string()
+                            }
+                        })
+                        .collect();
+
+                    let program = format!("(mod () {modern_sigil} {} (include sha256tree.clib) {} (sha256tree embedded-data))", embed_pt[0], embed_pt[1]);
+                    eprintln!("program {program}");
+
+                    let compiled = do_basic_run(&vec![
+                        "run".to_string(),
+                        "-i".to_string(),
+                        "resources/tests".to_string(),
+                        program,
+                    ]);
+
+                    if exists > 0 {
+                        let output =
+                            do_basic_brun(&vec!["brun".to_string(), compiled, "()".to_string()])
+                                .trim()
+                                .to_string();
+                        assert_eq!(want_hash, &output);
+                    } else {
+                        if modern > 0 {
+                            assert!(compiled.contains("could not find not-exist.txt"));
+                        } else {
+                            assert!(compiled.starts_with("FAIL:"));
+                        }
                     }
                 }
             }
