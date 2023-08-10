f21907046503b24d90a37381cb127a5ef6c82dad
arty <art.yerkes@gmail.com>
2023-08-10 14:59:01 -0700

    Get the tools loaded

--- wasm/tests/clvm-tools-interface/src/lib/tests/index.test.ts
+++ wasm/tests/clvm-tools-interface/src/lib/tests/index.test.ts
@@ -1,5 +1,6 @@
 import MyLibrary from '../index';
 import * as bls_loader from 'bls-signatures';
+var clvm_tools_rs = require('../../../../../pkg/clvm_tools_wasm');
 
 it('Runs without crashing', () => {
   new MyLibrary();
@@ -9,4 +10,5 @@ it('Has BLS signatures support', async () => {
     let bls = await bls_loader.default();
     let g1element = new bls.G1Element();
     console.log(g1element.serialize());
+    console.log(clvm_tools_rs);
 });
