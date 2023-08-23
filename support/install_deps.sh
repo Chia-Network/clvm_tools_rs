2d386c89b26e85f09f583fe47273097419734ff8
Adam Kelly <338792+aqk@users.noreply.github.com>
2023-08-23 12:27:02 -0700

    Make sure to use the correct (newer) version of clvm_tools_rs dependencies

---
+++ support/install_deps.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script is called from $GIT_ROOT/.github/workflows/build-test.yml
+# This script is called while in $GIT_ROOT/chia-blockchain of clvm_tools_rs
+
+. ./activate
+
+python -m pip uninstall clvm clvm_rs clvm_tools clvm_tools_rs
+
+git clone https://github.com/Chia-Network/clvm.git --branch=main --single-branch
+python -m pip install ./clvm
+
+echo "installing clvm_rs via pip"
+pip install clvm_rs
+
+echo "installing clvm_tools for clvm tests"
+
+# Ensure clvm_tools is installed from its own repo.
+git clone https://github.com/Chia-Network/clvm_tools.git --branch=main --single-branch
+python -m pip install ./clvm_tools
+
+# Install clvm_tools_rs from the directory above.
+python -m pip install ..
