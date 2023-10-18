2a69fd4e435568b7a08712bb1f7e7c50c4f5b46b
arty <art.yerkes@gmail.com>
2023-10-17 21:50:38 -0700

    clippy

--- src/compiler/preprocessor/mod.rs
+++ src/compiler/preprocessor/mod.rs
@@ -610,7 +610,7 @@ pub fn gather_dependencies(
 ) -> Result<Vec<IncludeDesc>, CompileErr> {
     let mut allocator = Allocator::new();
 
-    let assembled_input = assemble(&mut allocator, &file_content)
+    let assembled_input = assemble(&mut allocator, file_content)
         .map_err(|e| CompileErr(Srcloc::start(real_input_path), e.1))?;
     let dialect = detect_modern(&mut allocator, assembled_input);
     opts = opts.set_stdenv(dialect.strict).set_dialect(dialect.clone());
