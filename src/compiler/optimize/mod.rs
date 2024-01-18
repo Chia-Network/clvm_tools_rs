268235170142599e24adc7d1d043dc8e1a39b2f3
arty <art.yerkes@gmail.com>
2024-01-18 09:01:30 -0800

    opps

--- src/compiler/optimize/mod.rs
+++ src/compiler/optimize/mod.rs
@@ -374,7 +374,6 @@ fn constant_fun_result(
                 // we constructed above.
                 None,
             );
-            eprintln!("new_body {}", new_body.to_sexp());
 
             return Some(Rc::new(new_body));
         }
