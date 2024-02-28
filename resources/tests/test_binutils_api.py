88257675debf5395014ba5d2681bfd5df8b0bbbe
arty <art.yerkes@gmail.com>
2024-02-28 13:32:51 -0800

    Add a basic conversion for clvm_tools_rs' assemble using external cons and from_bytes methods.  This allows us to generate compatible sexp objects for use in other python related tasks.

--- resources/tests/test_binutils_api.py
+++ resources/tests/test_binutils_api.py
@@ -1,8 +1,21 @@
 from clvm_tools_rs import binutils
 from clvm_tools.binutils import assemble
+from clvm_rs.program import Program
 
-expected = assemble("(q)")
-assembled = binutils.assemble("(q)")
+test_cases = [
+    "(q)",
+    "()",
+    "(1 . 2)",
+    "(() . ())",
+    "(1 2 3 . ())",
+    "(1 2 3)",
+    "((4 5 6) (7 8 9))",
+    "(i (q . 1) (q . 2) (q . 3))"
+];
 
-# assembled should return a clvm.SExp object, but currently just returns a string
-assert expected == assembled
+for t in test_cases:
+    expected = assemble(t)
+    assembled = binutils.assemble_generic(lambda x,y: Program.to((x,y)), Program.to, t)
+
+    # assembled should return a clvm.SExp object, but currently just returns a string
+    assert expected == assembled
--- src/py/binutils.rs
+++ src/py/binutils.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use crate::classic::clvm::__type_compatibility__::t;
 use crate::classic::clvm::serialize::{sexp_from_stream, SimpleCreateCLVMObject};
 use crate::classic::clvm::sexp::{sexp_as_bin, First, NodeSel, Rest, SelectNode, ThisNode};
@@ -5,22 +7,87 @@ use clvm_rs::allocator::{Allocator, NodePtr, SExp};
 use clvm_rs::reduction::EvalErr;
 
 use pyo3::prelude::*;
+use pyo3::exceptions::PyException;
+use pyo3::types::{PyBytes,PyTuple};
 
 use crate::classic::clvm_tools::binutils;
 use crate::classic::clvm_tools::ir::reader::read_ir;
 
+create_exception!(mymodule, ConvError, PyException);
+
+fn convert_to_external(allocator: &Allocator, cons: &PyAny, from_bytes: &PyAny, node: NodePtr) -> PyResult<PyObject> {
+    let mut stack: Vec<NodePtr> = vec![node];
+    let mut finished = HashMap::new();
+    let mut conv_map = HashMap::new();
+
+    while let Some(n) = stack.pop() {
+        match allocator.sexp(n) {
+            SExp::Pair(a,b) => {
+                stack.push(a);
+                stack.push(b);
+                conv_map.insert(n, (a, b));
+            }
+            SExp::Atom => {
+                let converted: PyObject =
+                    Python::with_gil(|py| {
+                        let bytes = PyBytes::new(py, allocator.atom(n));
+                        let args = PyTuple::new(py, &[bytes]);
+                        from_bytes.call1(args)
+                    })?.into();
+                finished.insert(n, converted);
+            }
+        }
+    }
+
+    let check_cvt_map = |conv_map: &HashMap<NodePtr, (NodePtr, NodePtr)>, n| -> Option<(NodePtr, NodePtr)> {
+        if let Some(res) = conv_map.get(&n) {
+            return Some(res.clone());
+        }
+
+        None
+    };
+
+    // Convert conses.
+    stack.push(node);
+    while let Some(n) = stack.pop() {
+        if let Some(res) = finished.get(&n) {
+            continue;
+        }
+        if let Some((a, b)) = check_cvt_map(&conv_map, n) {
+            if let (Some(a), Some(b)) = (finished.get(&a), finished.get(&b)) {
+                let result: PyObject = Python::with_gil(|py| {
+                    let args = PyTuple::new(py, &[&a, &b]);
+                    cons.call1(args)
+                })?.into();
+                conv_map.remove(&n);
+                finished.insert(n, result);
+                continue;
+            }
+
+            stack.push(n);
+            stack.push(a);
+            stack.push(b);
+        }
+    }
+
+    if let Some(res) = finished.get(&node) {
+        return Ok(res.clone());
+    } else {
+        return Err(ConvError::new_err("error converting assembled value"));
+    }
+}
+
 #[pyfunction]
-pub fn assemble(args: String) -> PyResult<String> {
+pub fn assemble_generic(cons: &PyAny, from_bytes: &PyAny, args: String) -> PyResult<PyObject>
+{
     let mut allocator = Allocator::new();
     let assembled = binutils::assemble(&mut allocator, &args)
-        .map_err(|e| e.to_string())
-        .map(|sexp| t(sexp, sexp_as_bin(&mut allocator, sexp).hex()))
-        .unwrap();
-    Ok(assembled.rest().to_string())
+        .map_err(|e| ConvError::new_err(e.to_string()))?;
+    convert_to_external(&allocator, cons, from_bytes, assembled)
 }
 
 pub fn create_binutils_module(py: Python) -> PyResult<&'_ PyModule> {
     let m = PyModule::new(py, "binutils")?;
-    m.add_function(wrap_pyfunction!(assemble, m)?)?;
+    m.add_function(wrap_pyfunction!(assemble_generic, m)?)?;
     Ok(m)
 }
