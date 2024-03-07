2e64a6d8569f99d16a33052d2f330315c61341dc
arty <art.yerkes@gmail.com>
2024-03-06 18:36:11 -0800

    Add a simple property test generator.  not super efficient but decent at generating random stuff

--- resources/tests/test_binutils_api.py
+++ resources/tests/test_binutils_api.py
@@ -1,6 +1,7 @@
 from clvm_tools_rs import binutils
 from clvm_tools.binutils import assemble
 from clvm_rs.program import Program
+from random import randint
 
 test_cases = [
     "(q)",
@@ -11,11 +12,67 @@ test_cases = [
     "(1 2 3)",
     "((4 5 6) (7 8 9))",
     "(i (q . 1) (q . 2) (q . 3))"
-];
+]
+
+def extend_atom():
+    n_choice = randint(0,10)
+    if n_choice == 0:
+        return ""
+    return f"{chr(n_choice+97)}@"
+
+def pad(n,v,s):
+    if len(s) < n:
+        return "".join(([v]*(n - len(s)))) + s
+    else:
+        return s
+
+def extend_hex():
+    n_choice = randint(0,400)
+    if n_choice > 255:
+        return ""
+    return pad(2,"0",hex(n_choice)[2:])
+
+def any_item():
+    keys = list(filter(lambda k: k not in "&^", production_rules.keys()))
+    key_choice = randint(0,len(keys) - 1)
+    return keys[key_choice]
+
+production_rules = {
+    "!": lambda: "()",
+    "%": lambda: "(* . *)",
+    "~": lambda: "(*&",
+    "&": lambda: [")", " *&"][randint(0,1)],
+    "#": lambda: str(randint(0,2**48) - (2**47)),
+    "@": lambda: f"{chr(randint(0,10)+97)}{extend_atom()}",
+    "$": lambda: f"0x{pad(2,'0',hex(randint(0,255))[2:])}^",
+    "^": extend_hex,
+    "*": any_item
+}
+production_keys = "".join(production_rules.keys())
+
+def do_replacement(x):
+    if x in production_rules:
+        return production_rules[x]()
+    else:
+        return x
+
+def generate_random_sexp(s):
+    def generate_round(s):
+        return "".join([do_replacement(x) for x in s])
+
+    s = generate_round(s)
+    while any([x in production_keys for x in s]):
+        s = generate_round(s)
+
+    return s
+
+for i in range(20):
+    test_cases.append(generate_random_sexp("*"))
 
 for t in test_cases:
+    print(t)
     expected = assemble(t)
     assembled = binutils.assemble_generic(lambda x,y: Program.to((x,y)), Program.to, t)
+    print(expected, assembled)
 
-    # assembled should return a clvm.SExp object, but currently just returns a string
     assert expected == assembled
