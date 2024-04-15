966cc89e27956acb5d9883815ee8fdc4d2089719
arty <art.yerkes@gmail.com>
2024-04-15 15:43:14 -0700

    Filling in the final program production

--- src/tests/compiler/optimizer/cse_regression.rs
+++ src/tests/compiler/optimizer/cse_regression.rs
@@ -13,7 +13,7 @@ use crate::classic::clvm::__type_compatibility__::bi_one;
 use crate::classic::clvm_tools::stages::stage_0::{DefaultProgramRunner, TRunProgram};
 use crate::compiler::clvm::{convert_from_clvm_rs, run};
 use crate::compiler::compiler::DefaultCompilerOpts;
-use crate::compiler::comptypes::{BodyForm, CompileErr, CompilerOpts, DefconstData, DefunData, HelperForm};
+use crate::compiler::comptypes::{BodyForm, CompileErr, CompileForm, CompilerOpts, DefconstData, DefunData, HelperForm};
 use crate::compiler::dialect::AcceptedDialect;
 use crate::compiler::frontend::compile_helperform;
 use crate::compiler::fuzz::{ExprModifier, FuzzGenerator, FuzzTypeParams, Rule};
@@ -330,255 +330,71 @@ impl PropertyTestState<ExprCreationFuzzT> for ExprCreationState {
     }
 }
 
-struct ExprCreationGenerator {
-    rules: Vec<Rc<dyn Rule<ExprCreationFuzzT>>>
-}
+// Produce a program that provides a valid regression test for the cse merge fix.
+fn produce_valid_cse_regression_merge_test<R: Rng>(srcloc: &Srcloc, rng: &mut R) -> Option<CompileForm> {
+    // Strategy:
+    let vars = create_variable_set(srcloc.clone(), 7);
 
-struct TopExprBinopRule { op: SupportedOperators }
-impl Rule<ExprCreationFuzzT> for TopExprBinopRule {
-    fn check(
-        &self,
-        state: &mut <ExprCreationFuzzT as FuzzTypeParams>::State,
-        tag: &<ExprCreationFuzzT as FuzzTypeParams>::Tag,
-        idx: usize,
-        terminate: bool,
-        parents: &[<ExprCreationFuzzT as FuzzTypeParams>::Expr]
-    ) -> Result<Option<<ExprCreationFuzzT as FuzzTypeParams>::Expr>, <ExprCreationFuzzT as FuzzTypeParams>::Error> {
-        if tag != b"top" {
-            return Ok(None);
-        }
+    // Generate a definition graph including assign forms with fresh variables.
+    let structure_graph = create_structure_from_variables(rng, &vars);
 
-        todo!();
-    }
-}
+    // Generate fresh argument variables.
+    let args: Vec<Vec<u8>> = (0..5).map(|n| format!("a{n}").bytes().collect()).collect();
 
-impl ExprCreationGenerator {
-    fn new() -> Self {
-        // Make rules
-        ExprCreationGenerator {
-            rules: vec![
-                Rc::new(TopExprBinopRule { op: SupportedOperators::Plus }),
-            ]
-        }
+    // Get the generated variable graph.
+    let vars: Vec<Vec<u8>> = structure_graph.bindings.iter().map(|(k,v)| k.clone()).collect();
+
+    // Ensure this graph supports complex definitions, at least 2 vars share 1 in
+    // scope).
+    let vars_sharing_scopes: BTreeMap<Vec<u8>, Vec<Vec<u8>>> =
+        vars.iter().map(|v1| {
+            let v1_in_scope: BTreeSet<&Vec<u8>> =
+                structure_graph.variables_in_scope(v1).into_iter().collect();
+            (v1.clone(), vars.iter().filter(|v2| {
+                let v2_in_scope: BTreeSet<&Vec<u8>> = structure_graph.variables_in_scope(v2).into_iter().collect();
+                !v2_in_scope.intersection(&v1_in_scope).next().is_some()
+            }).cloned().collect::<Vec<Vec<u8>>>())
+        }).filter(|(k,v)| !v.is_empty()).collect();
+
+    if vars_sharing_scopes.is_empty() {
+        return None;
     }
 
-    fn generate<R: Rng>(&self, rng: &mut R) -> (ExprCreationState, Rc<ComplexAssignExpression>) {
-        let srcloc = Srcloc::start("*cl23-pre-cse-merge-fix");
-        let generated = PropertyTest::generate::<R, ExprCreationState>(rng, compose_sexp(srcloc.clone(), "${0:top}"), &self.rules);
-        todo!();
-    }
+    // For each variable in the graph, generate some candidate expressions to
+    // define it.
+    todo!();
+
+    //
+    // Based on random outcome, choose either to share one of the definitions or
+    // not.
+    //
+    // If sharing is chosen, choose two variables that share 1 other in scope.
+    // Generate a definition for one of them that is complex and compatible with
+    // the other's scope.
+    //
+    // If any variable's definition is compatible with the in-scope body of the
+    // definition graph, use it as the body.
+    //
+    // Emit and compile this program.
+    //
+    // If compilation succeeds in pre-fix cl23, then try in post-fix cl23.
+    // Assert that the produced program is the same.
 }
 
 #[test]
 fn test_cse_merge_regression() {
+
     let mut rng = simple_seeded_rng(13);
-    let eg = ExprCreationGenerator::new();
-    let (state, expression) = eg.generate(&mut rng);
+    let srcloc = Srcloc::start("*test*");
+
+    let produce_program = |rng: &mut ChaCha8Rng| {
+        loop {
+            if let Some(res) = produce_valid_cse_regression_merge_test(&srcloc, rng) {
+                return res;
+            }
+        }
+    };
+    let test_program = produce_program(&mut rng);
+    eprintln!("test_program {}", test_program.to_sexp());
     todo!();
 }
-
-/*
-struct TrickyAssignRegression {
-    opts: Rc<dyn CompilerOpts>,
-    loc: Srcloc,
-    count: usize,
-    actions: usize,
-    final_var: Option<Rc<ValueSpecification>>,
-    var_defs: BTreeMap<Vec<u8>, Rc<ValueSpecification>>,
-}
-
-impl HasVariableStore for TrickyAssignExpectation {
-    fn get(&self, name: &[u8]) -> Option<Rc<ValueSpecification>> {
-        self.var_defs.get(name).cloned()
-    }
-}
-
-impl TrickyAssignExpectation {
-    fn new(opts: Rc<dyn CompilerOpts>) -> Self {
-        TrickyAssignExpectation {
-            opts,
-            loc: Srcloc::start("*constant-test*"),
-            count: 0,
-            actions: 0,
-            final_var: None,
-            var_defs: BTreeMap::default(),
-        }
-    }
-
-    fn loc(&self) -> Srcloc {
-        self.loc.clone()
-    }
-
-    fn compute(&self) -> Rc<SExp> {
-        let val = self.final_var.as_ref().unwrap();
-        val.interpret(
-            self.opts.clone(),
-            &self.loc,
-            self,
-        )
-    }
-}
-
-struct FuzzT { }
-impl FuzzTypeParams for FuzzT {
-    type Tag = Vec<u8>;
-    type Expr = Rc<SExp>;
-    type Error = GenError;
-    type State = TrickyAssignExpectation;
-}
-
-struct TestTrickyAssignFuzzTopRule { defs: usize }
-impl Rule<FuzzT> for TestTrickyAssignFuzzTopRule {
-    fn check(&self, state: &mut TrickyAssignExpectation, tag: &Vec<u8>, idx: usize, terminate: bool, heritage: &[Rc<SExp>]) -> Result<Option<Rc<SExp>>,GenError> {
-        if tag != b"top" {
-            return Ok(None);
-        }
-
-        state.count = self.defs;
-        let start_program = compose_sexp(state.loc(), &format!("(mod (X) (include *standard-cl-23*) (defun F (X) (assign . ${{{}:assign-test-form}})) (F X))", idx));
-        Ok(Some(start_program.clone()))
-    }
-}
-
-struct TestTrickyAssignFuzzTestFormRule { }
-impl Rule<FuzzT> for TestTrickyAssignFuzzTestFormRule {
-    fn check(&self, state: &mut TrickyAssignExpectation, tag: &Vec<u8>, idx: usize, terminate: bool, heritage: &[Rc<SExp>]) -> Result<Option<Rc<SExp>>,GenError> {
-        if tag != b"assign-test-form" {
-            return Ok(None);
-        }
-
-        if state.count > 0 {
-            let varname = format!("v{}", state.count);
-            state.count -= 1;
-            Ok(Some(compose_sexp(state.loc(), &format!("({varname} ${{{idx}:vardef}} . ${{{}:assign-test-form}})", idx + 1))))
-        } else {
-            Ok(Some(compose_sexp(state.loc(), &format!("${{{idx}:final-expr}}"))))
-        }
-    }
-}
-
-fn find_var_name_in_heritage(heritage: &[Rc<SExp>]) -> Vec<u8> {
-    let NodeSel::Cons((varloc, varname), _) = NodeSel::Cons(AtomValue::Here(()), ThisNode).select_nodes(heritage[heritage.len() - 2].clone()).unwrap();
-    varname.clone()
-}
-
-struct TestTrickyAssignVarDefConstantRule { value: Rc<SExp> }
-impl Rule<FuzzT> for TestTrickyAssignVarDefConstantRule {
-    fn check(&self, state: &mut TrickyAssignExpectation, tag: &Vec<u8>, idx: usize, terminate: bool, heritage: &[Rc<SExp>]) -> Result<Option<Rc<SExp>>,GenError> {
-        if tag != b"vardef" || heritage.len() < 3 {
-            return Ok(None);
-        }
-
-        state.actions += 13;
-        let my_name = find_var_name_in_heritage(heritage);
-
-        let spec = Rc::new(ValueSpecification::ConstantValue(self.value.clone()));
-        state.var_defs.insert(my_name.clone(), spec);
-        Ok(Some(self.value.clone()))
-    }
-}
-
-struct TestTrickyAssignVarDefBinopRule { op: SupportedOperators, other: Rc<SExp> }
-impl Rule<FuzzT> for TestTrickyAssignVarDefBinopRule {
-    fn check(&self, state: &mut TrickyAssignExpectation, tag: &Vec<u8>, idx: usize, terminate: bool, heritage: &[Rc<SExp>]) -> Result<Option<Rc<SExp>>,GenError> {
-        if tag != b"vardef" || state.var_defs.is_empty() || heritage.len() < 3 {
-            return Ok(None);
-        }
-
-        state.actions += 13;
-        let my_name = find_var_name_in_heritage(heritage);
-
-        // We'll choose one other value and compose with the existing value.
-        let to_skip = state.actions % state.var_defs.len();
-        let (k, _) = state.var_defs.iter().skip(to_skip).next().unwrap();
-        let my_value = Rc::new(ValueSpecification::VarRef(k.to_vec()));
-
-        let spec = Rc::new(ValueSpecification::ClvmBinop(self.op.clone(), Rc::new(ValueSpecification::ConstantValue(self.other.clone())), my_value));
-        state.var_defs.insert(my_name.clone(), spec.clone());
-        Ok(Some(spec.to_sexp(&state.loc)))
-    }
-}
-
-struct TestTrickyAssignFinalExpr { }
-impl Rule <FuzzT> for TestTrickyAssignFinalExpr {
-    fn check(&self, state: &mut TrickyAssignExpectation, tag: &Vec<u8>, idx: usize, terminate: bool, heritage: &[Rc<SExp>]) -> Result<Option<Rc<SExp>>,GenError> {
-        if tag != b"final-expr" || state.var_defs.is_empty() {
-            return Ok(None);
-        }
-
-        let to_skip = state.actions % state.var_defs.len();
-        let (k, _) = state.var_defs.iter().skip(to_skip).next().unwrap();
-        state.final_var = Some(Rc::new(ValueSpecification::VarRef(k.to_vec())));
-        Ok(Some(Rc::new(SExp::Cons(state.loc.clone(), Rc::new(SExp::Atom(state.loc.clone(), k.to_vec())), Rc::new(SExp::Nil(state.loc.clone()))))))
-    }
-}
-
-struct TestTrickyAssignFinalBinopRule { op: SupportedOperators, other: Rc<SExp> }
-impl Rule <FuzzT> for TestTrickyAssignFinalBinopRule {
-    fn check(&self, state: &mut TrickyAssignExpectation, tag: &Vec<u8>, idx: usize, terminate: bool, heritage: &[Rc<SExp>]) -> Result<Option<Rc<SExp>>,GenError> {
-        if tag != b"final-expr" || state.var_defs.is_empty() {
-            return Ok(None);
-        }
-
-        let to_skip = state.actions % state.var_defs.len();
-        let (k, _) = state.var_defs.iter().skip(to_skip).next().unwrap();
-        let result_expr = Rc::new(ValueSpecification::ClvmBinop(self.op.clone(), Rc::new(ValueSpecification::ConstantValue(self.other.clone())), Rc::new(ValueSpecification::VarRef(k.to_vec()))));
-        state.final_var = Some(result_expr.clone());
-        Ok(Some(Rc::new(SExp::Cons(state.loc.clone(), result_expr.to_sexp(&state.loc), Rc::new(SExp::Nil(state.loc.clone()))))))
-    }
-}
-
-impl PropertyTestState for TrickyAssignExpectation {
-    fn new_state<R: Rng>(r: &mut R) -> Self {
-        let opts: Rc<dyn CompilerOpts> = Rc::new(DefaultCompilerOpts::new("*test*"));
-        TrickyAssignExpectation::new(opts.set_dialect(AcceptedDialect {
-            stepping: Some(23),
-            strict: true,
-        }).set_optimize(true))
-    }
-    fn filename(&self) -> String {
-        "*cl23-pre-cse-merge-fix-test.clsp".to_string()
-    }
-    fn run_args(&self) -> String { "(3)".to_string() }
-    fn check(&self, run_result: Rc<SExp>) {
-        let want_result = self.compute();
-        eprintln!("run_result {run_result} have {want_result}");
-        assert_eq!(run_result, want_result);
-    }
-}
-
-#[test]
-fn test_property_fuzz_cse_regression() {
-    let srcloc = Srcloc::start("*value*");
-    let mut rng = simple_seeded_rng(0x02020202);
-    let test = PropertyTest {
-        run_times: 500,
-        run_cutoff: 100,
-        run_expansion: 20,
-
-        top_node: compose_sexp(srcloc.clone(), "${0:top}"),
-        rules: vec![
-            Rc::new(TestTrickyAssignFuzzTopRule { defs: 1 }),
-            Rc::new(TestTrickyAssignFuzzTopRule { defs: 2 }),
-            Rc::new(TestTrickyAssignFuzzTopRule { defs: 3 }),
-            Rc::new(TestTrickyAssignFuzzTopRule { defs: 4 }),
-            Rc::new(TestTrickyAssignFuzzTopRule { defs: 5 }),
-            Rc::new(TestTrickyAssignFuzzTestFormRule { }),
-            Rc::new(TestTrickyAssignVarDefConstantRule {
-                value: compose_sexp(srcloc.clone(), "1")
-            }),
-            Rc::new(TestTrickyAssignVarDefBinopRule {
-                op: SupportedOperators::Times,
-                other: compose_sexp(srcloc.clone(), "2")
-            }),
-            Rc::new(TestTrickyAssignFinalExpr { }),
-            Rc::new(TestTrickyAssignFinalBinopRule {
-                op: SupportedOperators::Times,
-                other: compose_sexp(srcloc, "2")
-            }),
-        ]
-    };
-
-    test.run(&mut rng);
-}
-*/
