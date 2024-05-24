c5a2a285c6ba10bd926522d16d012f0ba877f195
arty <art.yerkes@gmail.com>
2024-05-24 09:11:59 -0700

    fmt + clippy

--- src/tests/classic/zero_constant_generation.rs
+++ src/tests/classic/zero_constant_generation.rs
@@ -6,25 +6,13 @@ fn test_fuzz_constant_gen() {
 
     let test_parallel_run = |test| {
         let final_program_classic = test_program.replace("%1", "").replace("%c", test);
-        let final_program_23_1 = test_program.replace("%1", "(include *standard-cl-23.1*)").replace("%c", test);
-        let classic = do_basic_run(&vec![
-            "run".to_string(),
-            final_program_classic
-        ]);
-        let new_ver = do_basic_run(&vec![
-            "run".to_string(),
-            final_program_23_1
-        ]);
-        let classic_output = do_basic_brun(&vec![
-            "brun".to_string(),
-            classic,
-            "0x01".to_string(),
-        ]);
-        let new_output = do_basic_brun(&vec![
-            "brun".to_string(),
-            new_ver,
-            "0x01".to_string(),
-        ]);
+        let final_program_23_1 = test_program
+            .replace("%1", "(include *standard-cl-23.1*)")
+            .replace("%c", test);
+        let classic = do_basic_run(&vec!["run".to_string(), final_program_classic]);
+        let new_ver = do_basic_run(&vec!["run".to_string(), final_program_23_1]);
+        let classic_output = do_basic_brun(&vec!["brun".to_string(), classic, "0x01".to_string()]);
+        let new_output = do_basic_brun(&vec!["brun".to_string(), new_ver, "0x01".to_string()]);
         assert_eq!(classic_output, new_output);
     };
 
@@ -41,7 +29,9 @@ fn test_fuzz_constant_gen() {
         "(concat 0x00 -129)",
         "(concat 0x00 (sha256 0x00 0))",
         "(concat 0x00 (sha256 0 0x00 0))",
-    ].iter() {
+    ]
+    .iter()
+    {
         test_parallel_run(test);
     }
 }
