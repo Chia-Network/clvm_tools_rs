43569f70207310d31ff2db3089b616f76b81f8a5
Earle Lowe <e.lowe@chia.net>
2025-01-07 14:06:21 -0800

    move python venv into script

--- resources/alpine/build-alpine.sh
+++ resources/alpine/build-alpine.sh
@@ -2,5 +2,7 @@
 
 . $HOME/.cargo/env
 cd /root/clvm_tools_rs
-source venv/bin/activate
+python3 -m venv venv
+source ./venv/bin/activate
+pip install maturin[patchelf]
 CC=gcc maturin build --release --strip --compatibility musllinux_1_1 --target x86_64-unknown-linux-musl
