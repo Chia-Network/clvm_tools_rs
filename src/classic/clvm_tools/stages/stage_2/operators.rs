c8b2c132f1c1bfac1efae8e41fe64b7f1e583f98
arty <art.yerkes@gmail.com>
2025-01-21 16:22:11 -0800

    fmt

--- src/classic/clvm_tools/stages/stage_2/operators.rs
+++ src/classic/clvm_tools/stages/stage_2/operators.rs
@@ -216,7 +216,9 @@ impl CompilerOperatorsInternal {
                     // Use the filesystem like normal if the opts couldn't find
                     // the file.
                     fs::read_to_string(&filename)
-                        .map_err(|_| EvalErr(NodePtr::NIL, format!("Failed to read file {filename}")))
+                        .map_err(|_| {
+                            EvalErr(NodePtr::NIL, format!("Failed to read file {filename}"))
+                        })
                         .and_then(|content| parse_file_content(allocator, &content))
                 }
                 _ => Err(EvalErr(NodePtr::NIL, "filename is not an atom".to_string())),
